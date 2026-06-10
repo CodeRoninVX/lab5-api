e4846a1e13960eaaf9cf2747cc3c36be30a031f1
Вахід <ahmedovvahid83@stud.op.edu.ua>
2026-06-10 15:03:29 +0300

    Prepare for cloud deployment: disable debug, dynamic PORT

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,6 @@ WORKDIR /app
 COPY . .
 RUN pip install -r requirements.txt
 ENV MODE="comfort"
+ENV AUTHOR="Akhмадзада Вахід"
+ENV GROUP="АІ-232"
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -100,8 +100,5 @@ if __name__ == '__main__':
     print("Сервер запущено: http://127.0.0.1:5000")
     print("Endpoint: POST http://127.0.0.1:5000/calculate")
 
-    app.run(
-        host='0.0.0.0',
-        port=5000,
-        debug=True
-    )
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
